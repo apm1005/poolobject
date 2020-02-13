c6d40d779a94e178cc2e8c6f933a8ee365b0bcf9
apm1005 <apm1005@alu.ubu.es>
2020-02-13 18:49:29 +0100

    Added setUp

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -5,21 +5,32 @@ package ubu.gii.dass.test.c01;
 
 import static org.junit.Assert.*;
 
+import java.util.Vector;
+import java.util.logging.Logger;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import ubu.gii.dass.c01.Reusable;
+import ubu.gii.dass.c01.ReusablePool;
 
 /**
  * @author alumno
  *
  */
 public class ReusablePoolTest {
+	
+	private Reusable r1, r2, r3;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		this.r1 = new Reusable();
+		this.r2 = new Reusable();
+		this.r3 = new Reusable();
 	}
 
 	/**
